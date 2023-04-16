a8eb873cd1dce536e308b429d5e276a1790aeba8
Laefye <rtpurtooe22@mail.ru>
2023-04-16 16:07:20 +0300

    Бомб лэнг

--- src/main/java/com/github/laefye/pixelbattle/tools/Bomb.java
+++ src/main/java/com/github/laefye/pixelbattle/tools/Bomb.java
@@ -1,5 +1,7 @@
 package com.github.laefye.pixelbattle.tools;
 
+import com.github.laefye.pixelbattle.Canvas;
+import com.github.laefye.pixelbattle.Color;
 import com.github.laefye.pixelbattle.Member;
 import com.github.laefye.pixelbattle.SomeConstants;
 import com.github.laefye.pixelbattle.abstracts.Tool;
@@ -21,7 +23,11 @@ public class Bomb extends Tool {
     public void updateInventory() {
         if (count > 0) {
             member.getInventory().setItem(SomeConstants.BOMB_SLOT, new ItemBuilder(Material.FIRE_CHARGE)
-                    .setDisplayName("bomba").setAmount(count).getItemStack());
+                            .setDisplayName(member.getPlugin().getLangConfig().getString("tool-bomb"))
+                            .setAmount(count)
+                            .setLore(member.getPlugin().getLangConfig().getString("tool-bomb-lore"))
+                            .getItemStack()
+            );
         } else {
             member.getInventory().clear(SomeConstants.BOMB_SLOT);
         }
@@ -39,28 +45,38 @@ public class Bomb extends Tool {
             return false;
         }
         var info = new SetInfo();
+        var result = 0;
+        result += set(canvas, x, y, z-2, color, info.copy().delay(6).triggerDynmap());
 
-        canvas.set(x, y, z-2, color, info.copy().delay(6).triggerDynmap());
+        result += set(canvas, x-1, y, z-1, color, info.copy().delay(4));
+        result += set(canvas, x, y, z-1, color, info.copy().delay(3));
+        result += set(canvas, x+1, y, z-1, color, info.copy().delay(4));
 
-        canvas.set(x-1, y, z-1, color, info.copy().delay(3));
-        canvas.set(x, y, z-1, color, info.copy().delay(3));
-        canvas.set(x+1, y, z-1, color, info.copy().delay(3));
+        result += set(canvas, x-2, y, z, color, info.copy().delay(6).triggerDynmap());
+        result += set(canvas, x-1, y, z, color, info.copy().delay(3));
+        result += set(canvas, x, y, z, color, new SetInfo().triggerDynmap());
+        result += set(canvas, x+1, y, z, color, info.copy().delay(3));
+        result += set(canvas, x+2, y, z, color, info.copy().delay(6).triggerDynmap());
 
-        canvas.set(x-2, y, z, color, info.copy().delay(6).triggerDynmap());
-        canvas.set(x-1, y, z, color, info.copy().delay(3));
-        canvas.set(x, y, z, color, new SetInfo().triggerDynmap());
-        canvas.set(x+1, y, z, color, info.copy().delay(3));
-        canvas.set(x+2, y, z, color, info.copy().delay(6).triggerDynmap());
+        result += set(canvas, x-1, y, z+1, color, info.copy().delay(4));
+        result += set(canvas, x, y, z+1, color, info.copy().delay(3));
+        result += set(canvas, x+1, y, z+1, color, info.copy().delay(4));
 
-        canvas.set(x-1, y, z+1, color, info.copy().delay(3));
-        canvas.set(x, y, z+1, color, info.copy().delay(3));
-        canvas.set(x+1, y, z+1, color, info.copy().delay(3));
+        result += set(canvas, x, y, z+2, color, info.copy().delay(6).triggerDynmap());
 
-        canvas.set(x, y, z+2, color, info.copy().delay(6).triggerDynmap());
-
-        count--;
+        if (result != 0) {
+            count--;
+        }
         updateInventory();
-        member.place(13);
-        return true;
+        member.place(result);
+        return result != 0;
+    }
+
+    public int set(Canvas canvas, int x, int y, int z, Color color, SetInfo base) {
+        if (canvas.get(x, y, z) == color) {
+            return 0;
+        }
+        canvas.set(x, y, z, color, base);
+        return 1;
     }
 }
--- src/main/java/com/github/laefye/pixelbattle/wrappers/ItemBuilder.java
+++ src/main/java/com/github/laefye/pixelbattle/wrappers/ItemBuilder.java
@@ -4,6 +4,9 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class ItemBuilder {
     private ItemStack itemStack;
     private ItemMeta itemMeta;
@@ -27,6 +30,11 @@ public class ItemBuilder {
 
     public ItemBuilder setAmount(int amount) {
         itemStack.setAmount(amount);
+        return this;
+    }
+
+    public ItemBuilder setLore(String lore) {
+        itemMeta.setLore(Arrays.stream(lore.split("\n")).collect(Collectors.toList()));
         return this;
     }
 }
